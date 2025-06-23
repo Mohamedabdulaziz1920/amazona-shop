505ae377ff8ccbc881ffca4a93ca05c6193160d4
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-06-24 01:17:06 +0300

    Create purchase-receipt.tsx

---
+++ emails/purchase-receipt.tsx
@@ -0,0 +1,113 @@
+import {
+  Body,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components'
+
+import { formatCurrency } from '@/lib/utils'
+import { IOrder } from '@/lib/db/models/order.model'
+
+type PurchaseReceiptEmailProps = {
+  order: IOrder
+  siteUrl: string
+}
+
+const dateFormatter = new Intl.DateTimeFormat('en', { dateStyle: 'medium' })
+
+export default function PurchaseReceiptEmail({
+  order,
+  siteUrl,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>View order receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className='font-sans bg-white'>
+          <Container className='max-w-xl'>
+            <Heading>Purchase Receipt</Heading>
+            <Section>
+              <Row>
+                <Column>
+                  <Text className='mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4'>
+                    Order ID
+                  </Text>
+                  <Text className='mt-0 mr-4'>{order._id.toString()}</Text>
+                </Column>
+                <Column>
+                  <Text className='mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4'>
+                    Purchased On
+                  </Text>
+                  <Text className='mt-0 mr-4'>
+                    {dateFormatter.format(order.createdAt)}
+                  </Text>
+                </Column>
+                <Column>
+                  <Text className='mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4'>
+                    Price Paid
+                  </Text>
+                  <Text className='mt-0 mr-4'>
+                    {formatCurrency(order.totalPrice)}
+                  </Text>
+                </Column>
+              </Row>
+            </Section>
+            <Section className='border border-solid border-gray-500 rounded-lg p-4 md:p-6 my-4'>
+              {order.items.map((item) => (
+                <Row key={item.product} className='mt-8'>
+                  <Column className='w-20'>
+                    <Link href={`${siteUrl}/product/${item.slug}`}>
+                      <Img
+                        width='80'
+                        alt={item.name}
+                        className='rounded'
+                        src={
+                          item.image.startsWith('/')
+                            ? `${siteUrl}${item.image}`
+                            : item.image
+                        }
+                      />
+                    </Link>
+                  </Column>
+                  <Column className='align-top'>
+                    <Link href={`${siteUrl}/product/${item.slug}`}>
+                      <Text className='mx-2 my-0'>
+                        {item.name} x {item.quantity}
+                      </Text>
+                    </Link>
+                  </Column>
+                  <Column align='right' className='align-top'>
+                    <Text className='m-0 '>{formatCurrency(item.price)}</Text>
+                  </Column>
+                </Row>
+              ))}
+              {[
+                { name: 'Items', price: order.itemsPrice },
+                { name: 'Tax', price: order.taxPrice },
+                { name: 'Shipping', price: order.shippingPrice },
+                { name: 'Total', price: order.totalPrice },
+              ].map(({ name, price }) => (
+                <Row key={name} className='py-1'>
+                  <Column align='right'>{name}:</Column>
+                  <Column align='right' width={70} className='align-top'>
+                    <Text className='m-0'>{formatCurrency(price)}</Text>
+                  </Column>
+                </Row>
+              ))}
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  )
+}
